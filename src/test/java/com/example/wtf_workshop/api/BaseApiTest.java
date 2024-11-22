8d42770ca358db77838399e6a31109ae56ccb7a9
ozornoy <azornin92@gmail.com>
2024-11-23 00:25:34 +0500

    Add ServerAuthRequest and models:ServerAuthSettings, AuthModule, AuthModules

--- src/test/java/com/example/wtf_workshop/api/BaseApiTest.java
+++ src/test/java/com/example/wtf_workshop/api/BaseApiTest.java
@@ -1,6 +1,38 @@
 package com.example.wtf_workshop.api;
 
 import com.example.wtf_workshop.BaseTest;
+import com.example.wtf_workshop.api.models.AuthModules;
+import com.example.wtf_workshop.api.models.ServerAuthSettings;
+import com.example.wtf_workshop.api.requests.ServerAuthRequest;
+import com.example.wtf_workshop.api.spec.Specifications;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import static com.example.wtf_workshop.api.generators.TestDataGenerator.generate;
 
 public class BaseApiTest extends BaseTest {
+    private final ServerAuthRequest serverAuthRequest = new ServerAuthRequest(Specifications.superUserSpec());
+    private AuthModules authModules;
+    private boolean perProjectPermissions;
+
+    @BeforeSuite(alwaysRun = true)
+    public void setupServerAuthSettings() {
+        perProjectPermissions = serverAuthRequest.read().getPerProjectPermissions();
+
+        authModules = generate(AuthModules.class);
+
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                .perProjectPermissions(true)
+                .modules(authModules)
+                .build());
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void cleanUpServerAuthSettings() {
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                .perProjectPermissions(perProjectPermissions)
+                .modules(authModules)
+                .build());
+    }
+
 }
