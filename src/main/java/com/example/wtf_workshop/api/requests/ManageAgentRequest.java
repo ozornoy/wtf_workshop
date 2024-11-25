e399e7b660cbd28926a725563fecfc7572ba3472
ozornoy <azornin92@gmail.com>
2024-11-25 13:02:02 +0500

    FIX auth_agent request

--- src/main/java/com/example/wtf_workshop/api/requests/ManageAgentRequest.java
+++ src/main/java/com/example/wtf_workshop/api/requests/ManageAgentRequest.java
@@ -13,11 +13,12 @@ public class ManageAgentRequest {
         this.spec = spec;
     }
 
-    public void auth_agent(String authorize) {
+    public void auth_agent(Boolean authorize) {
         String agentAuthUrl = String.format(AGENT_AUTH_URL_TEMPLATE, "name:ip_" + Config.getProperty("agent_ip"));
         RestAssured.given()
                 .spec(spec)
                 .header("Content-Type", "text/plain")
+                .header("Accept", "")
                 .body(String.valueOf(authorize))
                 .put(agentAuthUrl)
                 .then().assertThat().statusCode(HttpStatus.SC_OK);
