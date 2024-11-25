27b24d39cbb0b0c3977ff936fb1dd20b2fc2b5c6
ozornoy <azornin92@gmail.com>
2024-11-25 13:43:45 +0500

    ADD agents model

--- src/main/java/com/example/wtf_workshop/api/requests/ManageAgentRequest.java
+++ src/main/java/com/example/wtf_workshop/api/requests/ManageAgentRequest.java
@@ -1,16 +1,27 @@
 package com.example.wtf_workshop.api.requests;
 
 import com.example.wtf_workshop.api.config.Config;
+import com.example.wtf_workshop.api.models.Agents;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
 
 public class ManageAgentRequest {
+    private static final String AGENTS_URL = "/app/rest/agents";
     private static final String AGENT_AUTH_URL_TEMPLATE = "/app/rest/agents/%s/authorized";
     private RequestSpecification spec;
 
     public ManageAgentRequest(RequestSpecification spec) {
         this.spec = spec;
+    }
+
+    public Agents get_agents() {
+        return RestAssured.given()
+                .spec(spec)
+                .get(AGENTS_URL)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(Agents.class);
+
     }
 
     public void auth_agent(String authorize) {
@@ -22,6 +33,5 @@ public class ManageAgentRequest {
                 .body(String.valueOf(authorize))
                 .put(agentAuthUrl)
                 .then().assertThat().statusCode(HttpStatus.SC_OK);
-
     }
 }
--- src/test/java/com/example/wtf_workshop/api/SetupAgentTest.java
+++ src/test/java/com/example/wtf_workshop/api/SetupAgentTest.java
@@ -4,10 +4,23 @@ import com.example.wtf_workshop.api.requests.ManageAgentRequest;
 import com.example.wtf_workshop.api.spec.Specifications;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+import static org.awaitility.Awaitility.await;
+
 
 public class SetupAgentTest extends BaseApiTest{
     @Test(groups = {"Setup"})
     public void authTeamCityAgentTest() {
-        new ManageAgentRequest(Specifications.superUserSpec()).auth_agent("true");
+        var agentRequester = new ManageAgentRequest(Specifications.superUserSpec());
+
+        await().atMost(30, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(() -> {
+            var agents = agentRequester.get_agents();
+            if (agents.getCount() == 0) {
+                return false;
+            }
+            return true;
+        });
+
+        agentRequester.auth_agent("true");
     }
 }
